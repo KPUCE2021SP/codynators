8b2380c1d798bf3e7c60a26b9c25e5d35db9ef58
KimHacne <86879099+KimHacne@users.noreply.github.com>
2021-07-29 22:40:49 +0900

    Update MainActivity.kt
    메인엑티비티 수정좀 했어유

--- CodynatorsFB/app/src/main/java/com/example/codynatorsfb/MainActivity.kt
+++ CodynatorsFB/app/src/main/java/com/example/codynatorsfb/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.codynatorsfb
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        placeList.setOnClickListener {
+            val intent = Intent(this,PlaceListActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fadein,R.anim.fadeout)
+        }
     }
+
 }
