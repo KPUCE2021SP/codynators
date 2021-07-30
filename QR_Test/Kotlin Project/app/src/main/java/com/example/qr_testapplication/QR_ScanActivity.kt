a6813e444562a2606ac69ec150f7b5c791a41cbc
khsexk <kohyunsuk98@gmail.com>
2021-07-30 18:56:58 +0900

    Activity 추가

--- QR_Test/Kotlin Project/app/src/main/java/com/example/qr_testapplication/QR_ScanActivity.kt
+++ QR_Test/Kotlin Project/app/src/main/java/com/example/qr_testapplication/QR_ScanActivity.kt
@@ -2,13 +2,10 @@ package com.example.qr_testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.zxing.integration.android.IntentIntegrator
 
-class MainActivity : AppCompatActivity() {
-    private val
-
+class QR_ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_scan)
     }
 }
