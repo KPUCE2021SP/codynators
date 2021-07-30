5cccc2f7f28446849fcacf3961baee03c0ab4f4a
Taeyong Kim <amico741@naver.com>
2021-07-31 00:12:40 +0900

    Firebase 객체받는 부분 변경

--- SummerProject/app/src/main/java/com/example/summerproject/LoginActivity.kt
+++ SummerProject/app/src/main/java/com/example/summerproject/LoginActivity.kt
@@ -1,23 +1,26 @@
 package com.example.summerproject
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
+
 /*
 * 로그인 구현  21.07.30 김태용
 *  Anko 라이브러리 사용*/
 class LoginActivity : AppCompatActivity() {
+    private var firebaseAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        firebaseAuth = FirebaseAuth.getInstance() // Firebase Auth 객체를 얻는 변수
 
         loginBtn.setOnClickListener { //로그인을 위한 버튼 리스너
             var userEmail = username.text.toString()
@@ -32,9 +35,9 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun doLogin(userEmail : String, password : String){ // 로그인 함수
-        Firebase.auth.signInWithEmailAndPassword(userEmail, password) // Firebase.auth - firebase의 인증 API를 제공하는 객체
-            .addOnCompleteListener(this){
+    private fun doLogin(userEmail: String, password: String){ // 로그인 함수
+        firebaseAuth?.signInWithEmailAndPassword(userEmail, password) // Firebase.auth - firebase의 인증 API를 제공하는 객체
+            ?.addOnCompleteListener(this){
                 if(it.isSuccessful){
                     startActivity<MainActivity>() // 로그인 성공시 MainActivity 실행
                     finish()
