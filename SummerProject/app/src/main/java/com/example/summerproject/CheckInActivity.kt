41eec8de72e2546bb873c24bfa971062a06d17a4
TAEYONG KIM <amico741@naver.com>
2021-08-03 19:54:33 +0900

    cloud firestore 이용하여 user입력 및 테이블 상태체크

--- SummerProject/app/src/main/java/com/example/summerproject/CheckInActivity.kt
+++ SummerProject/app/src/main/java/com/example/summerproject/CheckInActivity.kt
@@ -1,11 +1,16 @@
 package com.example.summerproject
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import java.util.*
@@ -24,6 +29,9 @@ class CheckInActivity : AppCompatActivity() {
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Unit {
         val result : IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        val db: FirebaseFirestore = Firebase.firestore
+        val itemsCollectionRef = db.collection("Table use Information") // Collection 이름
+
         if(result != null) {
             if(result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
@@ -33,12 +41,17 @@ class CheckInActivity : AppCompatActivity() {
 
 
                 // Firebase Realtime DB에 쓰기
-                var useTable : String = result.getContents()
-                var Table : TableData = TableData("", 1)
-                var Map : HashMap<String, Any> = HashMap<String, Any>()
-                Map.put(useTable, Table)
+//                var useTable : String = result.getContents()
+//                var Table : TableData = TableData("", 1)
+//                var Map : HashMap<String, Any> = HashMap<String, Any>()
+//                Map.put(useTable, Table)
+//
+//                myRef.updateChildren(Map)
 
-                myRef.updateChildren(Map)
+                //Firebase Cloude Store에 useInfo 값 update : written by 태용
+                itemsCollectionRef.document("Table 1").update("useInfo", true).addOnSuccessListener {// 체크인 ACTIVITY들어왔을 시, useInfo가 true로 변경되는지 체크
+                    Log.d(ContentValues.TAG, "Update successfully written!")
+                }
                 finish()
             }
         } else {
--- SummerProject/app/src/main/java/com/example/summerproject/MainActivity.kt
+++ SummerProject/app/src/main/java/com/example/summerproject/MainActivity.kt
@@ -1,32 +1,58 @@
 package com.example.summerproject
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : Activity() {
     // 2021.08.01 khsexk: rdb 연동
-    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
-    val myRef : DatabaseReference = database.getReference("Table Use Information")
-
+//    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+//    val myRef : DatabaseReference = database.getReference("Table Use Information") made by 현석
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // 2021.08.01 khsexk: rdb 초기 데이터 생성 -> 생성후 삭제코드
-        var Table : TableData = TableData("", 0)
-        var Map : HashMap<String, Any> = HashMap<String, Any>()
-        Map.put("Table 1", Table)
-        Map.put("Table 2", Table)
-        Map.put("Table 3", Table)
-        myRef.updateChildren(Map)
+//        var Table : TableData = TableData("", 0)
+//        var Map : HashMap<String, Any> = HashMap<String, Any>()
+//        Map.put("Table 1", Table)
+//        Map.put("Table 2", Table)
+//        Map.put("Table 3", Table)
+//        myRef.updateChildren(Map) made by 현석
 
-        board.bringToFront()
+        /*Cloud Firestore 로 변경하기 위해 작업*/
+        val db: FirebaseFirestore = Firebase.firestore // : written by 태용
+        val array : ArrayList<String> = arrayListOf("Table 1", "Table 2", "Table 3") // 테이블 3개로 테스트 : written by 태용
+
+        val itemsCollectionRef = db.collection("Table use Information") // Collection 이름 : written by 태용
+//
+        val itemMap = hashMapOf( // to 앞의 값은 Key, to 뒤의 값은 value : written by 태용
+                "userID" to "${FirebaseAuth.getInstance().currentUser}",
+                "useInfo" to false,
+                "useTable" to " "
+        )
+
+        //board.bringToFront()
+
+        for(it in array){ // "Table use Information"이라는 컬렉션 밑에  table1, table2, table3 document 추가하고 각 document에 key : value 삽입
+            // : written by 태용
+            itemsCollectionRef.document(it)
+                    .set(itemMap)
+                    .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!")  }
+                    .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+        }
 
         // 2021.08.01 khsexk: 체크인 and 체크아웃
         checkIn.setOnClickListener{
