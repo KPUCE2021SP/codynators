134b7e58981e4322bd27a9912636b71703c52a10
eemdeeks <prince9088@gmail.com>
2021-08-13 18:57:56 +0900

    Update register(.kt , .xml , manifests)
    - 회원가입 수정( 프로필사진, 이름, RDB에 회원정보 저장)
    - 테마수정

--- SummerProject/app/src/main/java/com/example/summerproject/RegisterActivity.kt
+++ SummerProject/app/src/main/java/com/example/summerproject/RegisterActivity.kt
@@ -1,19 +1,28 @@
 package com.example.summerproject
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.startActivityForResult
 import org.jetbrains.anko.toast
+import java.util.*
 
 /*
 * 회원가입 구현  21.07.30 김태용 afterschool -weekely
+* RDB에 회원 정보 저장(이름,이미지추가) 21.08.13 - eemdeeks
 * Anko 라이브러리 사용*/
 class RegisterActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
@@ -22,6 +31,7 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         auth = Firebase.auth // // FireBase Auth 객체를 얻는 인스턴스
+
 
         Register.setOnClickListener { //회원가입
             var userEmail = Email.text.toString()
@@ -34,6 +44,9 @@ class RegisterActivity : AppCompatActivity() {
                 create.addOnCompleteListener(this) {
                     if (it.isSuccessful) {
                         toast("Register Success")
+
+                        uploadImageToFirebaseStorage() //프로필 이미지 RDB에 저장함수
+
                         finish()
                         overridePendingTransition(R.anim.fadein,R.anim.fadeout)
                     } else {
@@ -44,11 +57,80 @@ class RegisterActivity : AppCompatActivity() {
 
             //doSignUp(userEmail, password) // 회원가입 위한 함수
         }
+
+        //프로필 이미지 선택 버튼 구현
+        selectphoto_button_register.setOnClickListener {
+            Log.d("RegisterActivity","Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+
         btnCancel.setOnClickListener {
             finish()
             overridePendingTransition(R.anim.fadein,R.anim.fadeout)
         }
+
     }
+
+    var selectedPhotoUri: Uri? = null   //프로필 이미지 uri
+
+    //선택한 프로필 이미지 activity에 보이기
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode ==0 && resultCode == Activity.RESULT_OK && data !=null){
+
+            Log.d("RegisterActivity","Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,selectedPhotoUri)
+
+            selectphoto_imageview_register.setImageBitmap(bitmap)
+            selectphoto_button_register.alpha = 0f
+
+        //    val bitmapDrawable = BitmapDrawable(bitmap)
+        //    selectphoto_button_register.setBackgroundDrawable(bitmapDrawable)
+        }
+
+    }
+
+    //프로필 이미지 DB에서 불러오기
+    private fun uploadImageToFirebaseStorage(){
+        if(selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!).addOnSuccessListener {
+            Log.d("RegisterActivity", "Successfully uploaded image: ${it.metadata?.path}")
+
+            ref.downloadUrl.addOnSuccessListener {
+                it.toString()
+                Log.d("RegisterActivity","File Location: $it")
+
+                saveUserToFirebaseDatabase(it.toString())
+            }
+
+        }.addOnFailureListener{
+            //do some logging here
+        }
+    }
+
+    //이름, 아이디, 비밀번호, 프로필 이미지 RDB에 저장 함수
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String){
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, name_edt_register.text.toString(), profileImageUrl)
+
+        ref.setValue(user).addOnSuccessListener {
+            Log.d("RegisterActivity","Finally we saved the user to Firebase Database")
+        }
+    }
+
 //    private fun doSignUp(userEmail: String, password: String){ // 회원가입을 위한 함수
 //        auth.createUserWithEmailAndPassword(userEmail, password)
 //            .addOnCompleteListener(this){
@@ -63,3 +145,4 @@ class RegisterActivity : AppCompatActivity() {
 //            }
 //    } null 예외 처리가 안되어 클릭 리스너에서 구현
 }
+class User(val uid: String, val username: String, val profileImageUrl: String)
