ed321678baa92068f06baf01739227bdbd3e8d2b
khsexk <kohyunsuk98@gmail.com>
2021-08-13 23:43:40 +0900

    FSR-402 테스트
    LED를 연결하여 Serial 통신을 하면서 FSR-402 센서를 테스트했다.

--- arduino_test/fsr_test/fsr_test.ino
+++ arduino_test/fsr_test/fsr_test.ino
@@ -1,20 +1,22 @@
- #include <MsTimer2.h>
-
-const int THRESHOLD = 50;
-char ch = 'a';
-char ch1 = 'b';
-
-void setup() {
-  Serial.begin(9600); //시리얼 통신 초기화
+int sensorPin = 0;
+int ledPin = 9;
+ 
+void setup( ){
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+ 
 }
-
-void loop() {
-  int sensorValue = analogRead(A0); //압력센서로 부터 데이터를 읽는다.
-  if(sensorValue > THRESHOLD) { //값이 50 이상일 경우
-    Serial.write(ch); //계기판으로 데이터'a'를 전송
+ 
+void loop( ){
+  int value = analogRead(sensorPin);
+  int intensity = map(value, 0, 1024, 0, 255);
+  
+  Serial.println(intensity);
+  if(intensity>240){
+    analogWrite(ledPin, intensity);
+  }else{
+    analogWrite(ledPin, 0);
   }
-  else {
-    Serial.write(ch1); //자리에 없을 경우 데이터 'b'를 전송
-  }
-  delay(2000);  
+  delay(500);
+ 
 }
