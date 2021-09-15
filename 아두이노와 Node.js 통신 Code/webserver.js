6e928911bc8c862bda519c8d67b9fd3bdfd3b81a
TAEYONG KIM <amico741@naver.com>
2021-09-15 16:47:04 +0900

    아두이노 코드최신화

--- 아두이노와 Node.js 통신 Code/webserver.js
+++ 아두이노와 Node.js 통신 Code/webserver.js
@@ -1,9 +1,10 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var bodyParaser = require('body-parser');
 var SerialPort = require('serialport');
-<<<<<<< HEAD
 var fs = require('fs');
 var app = express();
-=======
->>>>>>> b2b7a32a856a60043aac1860713d78d1561bc7d1
 
 const parsers = SerialPort.parsers;
 const parser = new parsers.Readline(
@@ -12,14 +13,9 @@ const parser = new parsers.Readline(
     }
 );
 
-<<<<<<< HEAD
 
 var port = new SerialPort('COM3',{ // 사용하는 아두이노 Serial Port COM3 -> 아두이노에서 모니터 꺼야 노드에서 로그 출력됨
     baudRate: 9600, // 9600bps
-=======
-var port = new SerialPort('COM10',{
-    baudRate: 9600,
->>>>>>> b2b7a32a856a60043aac1860713d78d1561bc7d1
     dataBits: 8,
     parity: 'none',
     stopBits: 1,
@@ -31,18 +27,40 @@ port.pipe(parser); // parser 연결
 
 
 var table = 0;
-parser.on('data', function(data){
-    table = data;
-    console.log(table);
-});
 
 const tableData = {
     number: `${table}`
 };
 
-app.get('/', (req,res) => {
-    tableData.number = `${table}`
-    res.send(JSON.stringify(tableData))
+parser.on('data', function(data){
+    console.log(data);
+    // const promise = new Promise((resolve, reject)=>{
+    //     if(data){
+    //         resolve('사람 이용중');
+    //     }else{
+    //         reject('실패');
+    //     }
+    // });
+
+    // promise
+    //     .then((message) => {
+    //         // Firebase로 전송
+    //         console.log(message);
+    //     })
+    //     .catch((error) => {
+    //         console.error(error);
+    //     })
+    //     .finally(() => {
+    //         //console.log('promise 작동중');
+    //     })
+});
+
+
+
+
+app.get('/check', (req,res) => {
+    res.send('Hello');
+    console.log('Hello response 했음');
 });
 
 
