0a6aee34e573480e3dcbaea21e308f167dbcf237
Hyun Su <86879099+KimHacne@users.noreply.github.com>
2021-10-01 00:04:23 +0900

    공간 쥬씨만 추가

--- SummerProject/app/src/main/java/com/example/summerproject/PlaceListActivity.kt
+++ SummerProject/app/src/main/java/com/example/summerproject/PlaceListActivity.kt
@@ -3,17 +3,16 @@ package com.example.summerproject
 import android.app.Activity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_place_list.*
+import org.jetbrains.anko.startActivity
 
 class PlaceListActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_list)
 
-//        placeListToMain.bringToFront()
-//
-//        placeListToMain.setOnClickListener {
-//            finish()
-//            overridePendingTransition(R.anim.fadein,R.anim.fadeout)
-//        }
+        place_juciy.setOnClickListener {
+            startActivity<JuciyActivity>()
+            overridePendingTransition(R.anim.fadein,R.anim.fadeout)
+        }
     }
 }
